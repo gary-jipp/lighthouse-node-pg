763f415033a974ceb174f36830da62766111ed59
gary <github@accu.ca>
2024-03-30 20:10:20 -0700

    Update

--- index.js
+++ index.js
@@ -10,8 +10,27 @@ const config = {
 const pool = new pg.Pool(config);
 // const pool = new pg.Pool({connectionString});
 
-pool.query("select * from users")
-  .then(data => {
-    console.log(data.rows);
-    pool.end();
-  });
+const method = args[0];
+switch (method) {
+
+  case "all":
+    pool.query('select * from users')
+      .then(data => {
+        console.log(data.rows);  // We  care about rows. array
+        pool.end();
+      });
+    break;
+
+  case "show":
+    const id = args[1];
+    pool.query(`select * from users where id=${id}`)
+      .then(data => {
+        console.log(data.rows[0]);  // only need 1st item
+        pool.end();
+      });
+    break;
+
+  default:
+    console.log("No method provided");
+    break;
+}
